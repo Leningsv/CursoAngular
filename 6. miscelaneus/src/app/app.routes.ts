7d014c64d4154d355be9c4e9ba83e88d8726fe74
lenin <leningsv@hotmail.com>
2017-11-10 12:13:02 -0500

    Ultimos cambios

--- 6. miscelaneus/src/app/app.routes.ts
+++ 6. miscelaneus/src/app/app.routes.ts
@@ -3,20 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
 
-import { NewUserComponent } from './components/user/new-user.component';
-import { EditUserComponent } from './components/user/edit-user.component';
-import { DetailUserComponent } from './components/user/detail-user.component';
+import { USER_ROUTES } from './components/user/user.routes';
+
+// import { NewUserComponent } from './components/user/new-user.component';
+// import { EditUserComponent } from './components/user/edit-user.component';
+// import { DetailUserComponent } from './components/user/detail-user.component';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     {
         path: 'user/:id',
         component: UserComponent,
-        children: [
-            { path: 'newUser', component: NewUserComponent },
-            { path: 'editUser', component: EditUserComponent },
-            { path: 'detailUser', component: DetailUserComponent }
-        ]
+        children: USER_ROUTES
+        // children: [
+        //     { path: 'newUser', component: NewUserComponent },
+        //     { path: 'editUser', component: EditUserComponent },
+        //     { path: 'detailUser', component: DetailUserComponent },
+        //     { path: '**', pathMatch: 'full', redirectTo: 'newUser'}
+        // ]
     },
     { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
---
+++ 6. miscelaneus/src/app/components/user/user.routes.ts
@@ -0,0 +1,12 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { NewUserComponent } from './new-user.component';
+import { EditUserComponent } from './edit-user.component';
+import { DetailUserComponent } from './detail-user.component';
+
+export const USER_ROUTES: Routes = [
+    { path: 'newUser', component: NewUserComponent },
+    { path: 'editUser', component: EditUserComponent },
+    { path: 'detailUser', component: DetailUserComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'newUser'}
+];
