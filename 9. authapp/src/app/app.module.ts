f015ef4b82b6ea10217afe1c950a3918c24e166b
lenin <leningsv@hotmail.com>
2017-11-13 10:01:57 -0500

    Ultimos cambios Auth0

--- 9. authapp/src/app/app.module.ts
+++ 9. authapp/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { APP_ROUTING } from './app.routing';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PriceComponent } from './components/price/price.component';
+import { ProtectComponent } from './components/protect/protect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PriceComponent,
+    ProtectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
