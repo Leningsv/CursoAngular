9844fb1e460448ba0911f95343c243274397b248
lenin <leningsv@hotmail.com>
2017-11-13 10:19:09 -0500

    ultimos cambios

--- 9. authapp/src/app/app.module.ts
+++ 9. authapp/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
 import { APP_ROUTING } from './app.routing';
 
+// Servicios
+import { AuthService } from './services/auth.service';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -21,7 +26,9 @@ import { ProtectComponent } from './components/protect/protect.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ 9. authapp/src/app/services/auth.service.ts
@@ -0,0 +1,26 @@
+// src/app/auth/auth.service.ts
+
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import * as auth0 from 'auth0-js';
+
+@Injectable()
+export class AuthService {
+
+  auth0 = new auth0.WebAuth({
+    clientID: 'o4a558BPb1kKv1UIQTjgNZBIFw87xERT',
+    domain: 'leningsv.auth0.com',
+    responseType: 'token id_token',
+    audience: 'https://leningsv.auth0.com/userinfo',
+    redirectUri: 'http://localhost:4200/callback',
+    scope: 'openid'
+  });
+
+  constructor(public router: Router) {}
+
+  public login(): void {
+    this.auth0.authorize();
+  }
+
+}
